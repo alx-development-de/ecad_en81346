b2b35047788c701039d3d6974a69d3d992afe797
Alexander Thiel <alex@thiel-online.net>
2022-07-22 23:11:38 +0200

    Sort function for the ID elements implemented

--- lib/ALX/EN81346.pm
+++ lib/ALX/EN81346.pm
@@ -7,8 +7,9 @@ package ALX::EN81346;
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(
-					segments
-				);
+    segments,
+    sort
+);
 
 our $VERSION = '0.01';
 
@@ -18,55 +19,95 @@ use Carp;
 
 use Log::Log4perl ();
 use Log::Log4perl::Level ();
-use Data::Dumper::Perltidy;
 
 # Initializing the logging if not already specified by the application
 # which uses the module
-if (not Log::Log4perl->initialized()) {
-        Log::Log4perl->easy_init( Log::Log4perl::Level::to_priority( 'OFF' ) );
-    }
+BEGIN {
+	if (not Log::Log4perl->initialized()) {
+    	Log::Log4perl->easy_init(Log::Log4perl::Level::to_priority('OFF'));
+	}
+}
 
 sub segments($;) {
-	my $string = shift();
+    my $string = shift();
+	Log::Log4perl->get_logger->debug("Segmenting string value: [$string]");
 
-	my $logger = Log::Log4perl->get_logger();
-	$logger->debug("Segmenting string value: [$string]");
+    # Initializing the returned hash structure
+    my %segments;
 
-	# Initializing the returned hash structure
-	my %segments;
+    # Splitting the string into segments according the prefix
+    my @matches = $string =~ m/([+=-]+[0-9a-zA-Z.]+)/gi;
 
-	# Splitting the string into segments according the prefix
-	my @matches = $string =~ m/([+=-]+[0-9a-zA-Z.]+)/gi;
+    # Looking for subsegments in the separate segments and splitting
+    # them into individual segments
+    foreach (@matches) {
+        my ($identifier, $value) = $_ =~ m/([+=-]+)([0-9a-zA-Z.]+)+/gi;
+        #print "Segments identified   : [$identifier] - [$string]\n";
 
-	# Looking for subsegments in the separate segments and splitting
-	# them into individual segments
-	foreach (@matches){
-		my ($identifier, $value) = $_ =~ m/([+=-]+)([0-9a-zA-Z.]+)+/gi;
-		#print "Segments identified   : [$identifier] - [$string]\n";
+        # Subsegments must be split if the dot-notation is used
+        my @subsegments = $value =~ m/([0-9a-zA-Z]+)\.?/gi;
 
-		# Subsegments must be split if the dot-notation is used
-		my @subsegments = $value =~ m/([0-9a-zA-Z]+)\.?/gi;
-
-		# Building the structure for each identifier in a key-value pairing.
-		# As key the identifier is used and the value is an array with the
-		# sorted values as array
-		$segments{$identifier} = [] unless defined $segments{$identifier};
-		$segments{$identifier} = [@{$segments{$identifier}}, @subsegments];
-	}
-	# Returning the segment structure
-	return \%segments;
+        # Building the structure for each identifier in a key-value pairing.
+        # As key the identifier is used and the value is an array with the
+        # sorted values as array
+        $segments{$identifier} = [] unless defined $segments{$identifier};
+        $segments{$identifier} = [ @{$segments{$identifier}}, @subsegments ];
+    }
+    # Returning the segment structure
+    return \%segments;
 }
 
 sub to_string($;) {
-	# TODO: Not sure if this is the correct way to handle hash references
-	my %segments = %{shift()};
+    # TODO: Not sure if this is the correct way to handle hash references
+    my %segments = %{shift()};
 
-	my $string_representation = '';
-	# TODO: Need to implement some sorting algorithm for the identifiers
-	foreach my $key (keys(%segments)){
-		$string_representation .= $key.join('.', @{$segments{$key}})
-	};
-	return $string_representation;
+    my $string_representation = '';
+    foreach my $key (ALX::EN81346::sort(keys(%segments))) {
+        $string_representation .= $key . join('.', @{$segments{$key}})
+    };
+    return $string_representation;
+}
+
+sub sort {
+    # Return the sorted array of given IDs
+    return sort compare_id @_;
+}
+
+# compare two numbers
+sub compare_id {
+    my( $first, $second ) = ( $a, $b );
+
+	Log::Log4perl->get_logger->debug("Comparing input $first and $second");
+	my %id = ( ':' => 0, '-' => 1, '+' => 2, '=' => 3 );
+
+    # Transforming the literals to numeric values to sort them using
+    # simple number comparison
+	if ( $first =~ m/([+:=-]).*/g ) {
+        $first = $id{$1} +  length($first) * 0.001;
+        Log::Log4perl->get_logger->debug('Result of $x $1: '."[$1]->[$first]");
+    } else {
+        carp "Not a valid ID provided for comparison: [$first]";
+    }
+
+	if ( $second =~ m/([:+=-]).*/g ) {
+        $second = $id{$1} + length($second) * 0.001;
+        Log::Log4perl->get_logger->debug('Result of $y $1: '."[$1]->[$second]");
+    } else {
+        carp "Not a valid ID provided for comparison: [$second]";
+    }
+
+    # Doing the comparison if the transformation has been successfully finished
+    if( defined($first) && defined($second) ) {
+        Log::Log4perl->get_logger->debug("Comparing $first and $second");
+
+        # Doing the comparison
+        if    ( $first > $second ) { return -1; }
+        elsif ( $first < $second ) { return  1; }
+        elsif ( $first == $second ) { return 0; }
+
+    } else {
+        croak "ID Comparison failed!";
+    }
 }
 
 #------------------------------------------------------------------------------------------------
--- t/01-EN81346.t
+++ t/01-EN81346.t
@@ -2,38 +2,39 @@
 use strict;
 use warnings;
 use Test::More;
+use Data::Dumper;
 
 use Log::Log4perl;
 Log::Log4perl->easy_init( Log::Log4perl::Level::to_priority( 'OFF' ) );
 
 require_ok( 'ALX::EN81346' );
 # Testing the simple structure detection without duplicate identifiers (multi-level structures)
-is_deeply(ALX::EN81346::segments('=100+110-X1'), {
+is_deeply( ALX::EN81346::segments('=100+110-X1'), {
     '='  => [ '100' ],
     '+'  => [ '110' ],
     '-'  => [ 'X1' ]
 }, "EN81346 Simple structure");
-is_deeply(ALX::EN81346::segments('=100+110-X2'), {
+is_deeply( ALX::EN81346::segments('=100+110-X2'), {
     '='  => [ '100' ],
     '+'  => [ '110' ],
     '-'  => [ 'X2' ]
 }, "EN81346 Simple structure");
 # Testing multi-level structures with different approaches
-is_deeply(ALX::EN81346::segments('==AbC910==23AX=113=aBc+1CC01+CAB-X9-7'), {
+is_deeply( ALX::EN81346::segments('==AbC910==23AX=113=aBc+1CC01+CAB-X9-7'), {
     '==' => [ 'AbC910', '23AX' ],
     '='  => [ '113', 'aBc' ],
     '+'  => [ '1CC01', 'CAB' ],
     '-'  => [ 'X9', '7' ]
 }, "EN81346 deep structure with multiple identifiers")
     || diag explain ALX::EN81346::segments('==AbC910==23AX=113=aBc+1CC01+CAB-X9-7');
-is_deeply(ALX::EN81346::segments('==AbC910.23AX=113.aBc+1CC01.CAB-X9.7'), {
+is_deeply( ALX::EN81346::segments('==AbC910.23AX=113.aBc+1CC01.CAB-X9.7'), {
     '==' => [ 'AbC910', '23AX' ],
     '='  => [ '113', 'aBc' ],
     '+'  => [ '1CC01', 'CAB' ],
     '-'  => [ 'X9', '7' ]
 }, "EN81346 deep structure with dot separator")
     || diag explain ALX::EN81346::segments('==AbC910.23AX=113.aBc+1CC01.CAB-X9.7');
-is_deeply(ALX::EN81346::segments('==AbC910+1CC01=113+CAB-X9==23AX-7=aBc'), {
+is_deeply( ALX::EN81346::segments('==AbC910+1CC01=113+CAB-X9==23AX-7=aBc'), {
     '==' => [ 'AbC910', '23AX' ],
     '='  => [ '113', 'aBc' ],
     '+'  => [ '1CC01', 'CAB' ],
@@ -43,7 +44,7 @@ is_deeply(ALX::EN81346::segments('==AbC910+1CC01=113+CAB-X9==23AX-7=aBc'), {
 
 TODO: {
     local $TODO = "The connector pin must also been detected with the colon as identifier";
-    is_deeply(ALX::EN81346::segments('==FZ910=113++CAB+1CC01-X8.1:13'), {
+    is_deeply( ALX::EN81346::segments('==FZ910=113++CAB+1CC01-X8.1:13'), {
         '==' => [ 'FZ910' ],
         '++' => [ 'CAB' ],
         '='  => [ '113' ],
@@ -52,6 +53,28 @@ TODO: {
         ':'  => [ '13' ]
     }, "Connector pin detection with colon seperator")
         || diag explain ALX::EN81346::segments('==FZ910=113++CAB+1CC01-X8.1:13');
+}
+
+# Testing the sort implementation of the ID sorting
+my @tests = (
+    {
+        'unsorted' => ['-', '+', '='],
+        'sorted'   => ['=', '+', '-']
+    },
+    {
+        'unsorted' => ['-', '--', '---'],
+        'sorted'   => ['---', '--', '-']
+    },
+    {
+        'unsorted' => ['+', '=', '==', '-', ':', '++++++++', '--', '---', '========'],
+        'sorted'   => ['========', '==', '=', '++++++++', '+', '---', '--', '-', ':']
+    }
+);
+for my $i (0 .. $#tests)
+{
+    my %test= %{$tests[$i]};
+    my @sorted =  ALX::EN81346::sort(@{$test{'unsorted'}});
+    is_deeply(\@sorted, \@{$test{'sorted'}}, 'ID sorting algorithm');
 }
 
 done_testing();
